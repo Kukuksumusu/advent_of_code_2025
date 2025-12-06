1467a98da2d7ac6bdd8f44578b96d058ae73fee2
Jakub Stercl <jakub.stercl@statsperform.com>
2025-12-06 16:46:53 +0100

    feat: day03

---
+++ src/day03/task1.py
@@ -0,0 +1,19 @@
+def get_joltage(bank: str) -> int:
+    first_digit = 0
+    second_digit = 0
+    for i in range(len(bank)):
+        digit = int(bank[i])
+        if digit > first_digit and i < len(bank) - 1:
+            first_digit = digit
+            second_digit = int(bank[i + 1])
+        elif digit > second_digit:
+            second_digit = digit
+
+    return first_digit * 10 + second_digit
+
+def solve(input_data: str) -> int:
+    banks = input_data.strip().splitlines()
+    res = 0
+    for bank in banks:
+        res += get_joltage(bank)
+    return res
---
+++ src/day03/task2.py
@@ -0,0 +1,29 @@
+
+TARGET_LEN = 12
+
+
+def find_highest_digit(bank: str) -> tuple[int, int]:
+    highest_index = 0
+    highest_digit = 0
+    for i in range(len(bank)):
+        if int(bank[i]) > highest_digit:
+            highest_digit = int(bank[i])
+            highest_index = i
+    return highest_digit, highest_index
+
+
+def get_joltage(bank: str) -> int:
+    res = 0
+    for i in range(TARGET_LEN):
+        end_index = -TARGET_LEN + i + 1
+        highest_digit, highest_index = find_highest_digit(bank[:end_index] if end_index < 0 else bank)
+        res += highest_digit * 10 ** (-end_index)
+        bank = bank[highest_index + 1:]
+    return res
+
+def solve(input_data: str) -> int:
+    banks = input_data.strip().splitlines()
+    res = 0
+    for bank in banks:
+        res += get_joltage(bank)
+    return res
